213a50537f65f7f0f34081fb131e7cedb65bdf68
Kevin SuTJ <shengsu15@gmail.com>
2022-11-21 15:21:19 +0800

    clean code

--- app/src/main/java/cn/neday/android/template/di/HttpClientModule.kt
+++ app/src/main/java/cn/neday/android/template/di/HttpClientModule.kt
@@ -3,11 +3,8 @@ package cn.neday.android.template.di
 import cn.neday.android.template.BASE_URL
 import cn.neday.android.template.TIME_OUT_SECONDS
 import cn.neday.android.template.network.interceptor.AuthenticationInterceptor
-import cn.neday.base.di.namedChuckerInterceptor
-import cn.neday.base.di.namedHttpLoggingInterceptor
-import okhttp3.Interceptor
+import cn.neday.base.network.interceptor.HttpInterceptorModule
 import okhttp3.OkHttpClient
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -32,8 +29,8 @@ val httpClientModule = module {
             .connectTimeout(TIME_OUT_SECONDS, TimeUnit.SECONDS)
             .readTimeout(TIME_OUT_SECONDS, TimeUnit.SECONDS)
             .addInterceptor(AuthenticationInterceptor())
-            .addInterceptor(get(qualifier = named(namedHttpLoggingInterceptor)) as Interceptor)
-            .addInterceptor(get(qualifier = named(namedChuckerInterceptor)) as Interceptor)
+            .addInterceptor(HttpInterceptorModule.httpLoggingInterceptor())
+            .addInterceptor(HttpInterceptorModule.chuckerInterceptor())
             .build()
     }
 }
--- base/src/main/java/cn/neday/base/network/interceptor/HttpInterceptor.kt
+++ base/src/main/java/cn/neday/base/network/interceptor/HttpInterceptor.kt
@@ -1,20 +1,15 @@
-package cn.neday.base.di
+package cn.neday.base.network.interceptor
 
 import cn.neday.base.BuildConfig
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.didichuxing.doraemonkit.util.Utils
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.core.qualifier.named
-import org.koin.dsl.module
 
-const val namedChuckerInterceptor = "ChuckerInterceptor";
-const val namedHttpLoggingInterceptor = "HttpLoggingInterceptor";
+object HttpInterceptorModule {
 
-val httpInterceptorModule = module {
-
-    single(named(namedChuckerInterceptor)) {
-        ChuckerInterceptor.Builder(Utils.getApp())
+    fun chuckerInterceptor(): ChuckerInterceptor {
+        return ChuckerInterceptor.Builder(Utils.getApp())
             .collector(ChuckerCollector(Utils.getApp()))
             .maxContentLength(250000L)
             .redactHeaders(emptySet())
@@ -22,8 +17,8 @@ val httpInterceptorModule = module {
             .build()
     }
 
-    single(named(namedHttpLoggingInterceptor)) {
-        HttpLoggingInterceptor().apply {
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
             level = when (BuildConfig.DEBUG) {
                 true -> HttpLoggingInterceptor.Level.BODY
                 false -> HttpLoggingInterceptor.Level.NONE
