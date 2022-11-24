3d8713ab121471c9f9e273616519bc87c057e8ef
Kevin SuTJ <shengsu15@gmail.com>
2022-11-24 10:05:18 +0800

    clean code

--- app/src/main/java/cn/neday/android/template/activity/MainActivity.kt
+++ app/src/main/java/cn/neday/android/template/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package cn.neday.android.template.activity
 
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import cn.neday.android.template.R
@@ -11,9 +10,6 @@ import cn.neday.base.activity.BaseBindingActivity
 class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
-        // Handle the splash screen transition.
-        val splashScreen = installSplashScreen()
-
         setSupportActionBar(binding.toolbar)
         val navController =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
